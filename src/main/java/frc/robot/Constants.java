0834699c4f4df463852a8501ebaa4d3b5a940d73
ev118 <92764232+ev118@users.noreply.github.com>
2024-12-14 13:58:05 -0800

    increased slowmode

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -110,7 +110,7 @@ public final class Constants {
   public static class DrivetrainConfig {
     public static final double MAX_DRIVE_SPEED = 1.7;
     public static final double MAX_TURN_SPEED = 1.0;
-    public static final double SLOWMODE_MULTIPLIER = 0.3;
+    public static final double SLOWMODE_MULTIPLIER = 0.45;
     public static final PIDController DRIVETRAIN_PID_CONTROLLER = new PIDController(0.3, 0, 0);
   }
 
