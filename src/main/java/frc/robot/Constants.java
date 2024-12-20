51f6fee0db1f4ce9a72c86e27adf7f56c99e11e5
ev118 <evenn118@gmail.com>
2024-12-19 16:02:48 -0800

    minor formatting changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -138,10 +138,10 @@ public final class Constants {
 
     public static final Transform2d TAG_TOTE_TRANSFORM_1 = new Transform2d(
         0,
-        -Units.inchesToMeters(44.5),
+        Units.inchesToMeters(-36.61),
         new Rotation2d());
     public static final Transform2d TAG_TOTE_TRANSFORM_2 = new Transform2d(
-        -Units.inchesToMeters(1.5),
+        Units.inchesToMeters(-19.28),
         Units.inchesToMeters(6.0),
         new Rotation2d(Units.degreesToRadians(-90)));
   }
--- src/main/java/frc/robot/commands/RunAuto.java
+++ src/main/java/frc/robot/commands/RunAuto.java
@@ -14,20 +14,17 @@ import frc.robot.subsystems.Vision;
 public class RunAuto extends SequentialCommandGroup {
   public RunAuto(Drivetrain drivetrain, Grabber grabber, Intake intake, Vision vision, AutoRoutine auto) {
     addCommands(
-      new DriveToPose(drivetrain, auto.transform),
-      new DriveToTarget(drivetrain, vision),
-      new ZeroGrabberArm(grabber)
-    );
+        new DriveToPose(drivetrain, auto.transform),
+        new DriveToTarget(drivetrain, vision),
+        new ZeroGrabberArm(grabber));
 
     if (auto != AutoRoutine.LEFT_LOW && auto != AutoRoutine.RIGHT_LOW) {
-      // TODO need locations of camera and grabber on robot
       addCommands(
-        new DriveToPose(drivetrain, AutoConfig.TAG_TOTE_TRANSFORM_1),
-        new DriveToPose(drivetrain, AutoConfig.TAG_TOTE_TRANSFORM_2),
-        drivetrain.driveCommand(0, 0.01 * DrivetrainConfig.MAX_DRIVE_SPEED, 0),
-        new PickUpTote(grabber),
-        drivetrain.driveCommand(0, 0, 0)
-      );
+          new DriveToPose(drivetrain, AutoConfig.TAG_TOTE_TRANSFORM_1),
+          new DriveToPose(drivetrain, AutoConfig.TAG_TOTE_TRANSFORM_2),
+          drivetrain.driveCommand(0, 0.01 * DrivetrainConfig.MAX_DRIVE_SPEED, 0),
+          new PickUpTote(grabber),
+          drivetrain.driveCommand(0, 0, 0));
     }
 
     addCommands(intake.toggleForwards(), new WaitCommand(1.5), intake.stopIntake());
